50a76dbf295771ecf3a91364e98fb2c96c6765b3
Arthur Bogaart <spikylee@gmail.com>
2015-07-07 01:22:03 +0200

    Mocha Chai tests for common/files/strings

---
+++ test/files_test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+var files = require('../lib/util/files.js');
+var path  = require('path');
+var sh    = require('shelljs');
+
+describe('File utils', function() {
+  it('exists', function() {
+    expect(files.exists(__dirname)).to.be.true;
+    expect(files.exists(__filename)).to.be.true;
+    expect(files.exists(path.join(__dirname, 'non-existing'))).to.not.be.true;
+    expect(files.exists(path.join(__dirname, 'non-existing.txt'))).to.not.be.true;
+  });
+
+  it('file exists', function() {
+    expect(files.fileExists(__filename)).to.be.true;
+    expect(files.fileExists('non-existing')).to.not.be.true;
+    expect(files.fileExists('non-existing.txt')).to.not.be.true;
+  });
+
+  it('folder exists', function() {
+    expect(files.folderExists(__dirname)).to.be.true;
+    expect(files.folderExists('non-existing')).to.not.be.true;
+    expect(files.folderExists('non-existing.txt')).to.not.be.true;
+  });
+
+  it('read non-existing file', function() {
+    expect(files.readFileIfExists('non-existing')).to.not.be.true;
+    expect(files.readFileIfExists('non-existing.txt')).to.not.be.true;
+    expect(files.readFileIfExists('non-existing/file')).to.not.be.true;
+    expect(files.readFileIfExists('non-existing/file.xml')).to.not.be.true;
+  });
+
+  it('read existing file', function() {
+    expect(files.readFileIfExists('./test/resources/test.json')).to.equal('{\n  "id": "test-file"\n}');
+  });
+
+  it('read non-existing json file', function() {
+    expect(files.readJSON('non-existing')).to.deep.equal({});
+    expect(files.readJSON('non-existing.json')).to.deep.equal({});
+  });
+
+  it('read non-existing json file', function() {
+    expect(files.readJSON('non-existing')).to.deep.equal({});
+    expect(files.readJSON('non-existing.json')).to.deep.equal({});
+  });
+
+  it('read existing json file', function() {
+    expect(files.readJSON('./test/resources/test.json')).to.deep.equal({id: 'test-file'});
+  });
+
+  it('write json file', function() {
+    var file = './test/resources/tmp.json';
+    var json = {id: 'write-json-file'};
+    files.writeJSON(json, file);
+    expect(files.readJSON(file)).to.deep.equal(json);
+
+    // remove tmp file
+    sh.rm(file);
+  });
+
+});
---
+++ test/strings_test.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var expect  = require('chai').expect;
+var strings = require('../lib/util/strings.js');
+var chalk   = require('chalk');
+
+describe('String utils', function() {
+  it('string is empty', function() {
+    expect(strings.isEmpty(undefined)).to.be.true;
+    expect(strings.isEmpty(null)).to.be.true;
+    expect(strings.isEmpty('')).to.be.true;
+    expect(strings.isEmpty(' ')).to.not.be.true;
+  });
+
+  it('string is blank', function() {
+    expect(strings.isBlank(undefined)).to.be.true;
+    expect(strings.isBlank(null)).to.be.true;
+    expect(strings.isBlank('')).to.be.true;
+    expect(strings.isBlank(' ')).to.be.true;
+    expect(strings.isBlank('  ')).to.be.true;
+    expect(strings.isBlank('  s')).to.not.be.true;
+  });
+
+  it('colored', function() {
+    var blue = chalk.blue;
+    expect(strings.colored()).to.equal(undefined);
+    expect(strings.colored(blue)).to.equal(undefined);
+    expect(strings.colored(blue, [])).to.deep.equal([]);
+    expect(strings.colored(blue, ['test'])).to.deep.equal([blue('test')]);
+    expect(strings.colored(blue, ['test1', 'test2'])).to.deep.equal([blue('test1'), blue('test2')]);
+    expect(strings.colored(blue, ['test1', chalk.yellow('test2')]))
+      .to.deep.equal([blue('test1'), chalk.yellow('test2')]);
+  });
+
+  it('translate empty values', function() {
+    expect(strings.translate(undefined)).to.equal('');
+    expect(strings.translate(null)).to.equal('');
+    expect(strings.translate('')).to.equal('');
+    expect(strings.translate(' ')).to.equal(' ');
+  });
+
+  it('translate with colored parameters', function() {
+    expect(strings.translate('hello')).to.equal('hello');
+    expect(strings.translate('hello %s', 'world')).to.equal('hello ' + chalk.red('world'));
+    expect(strings.translate('hello %s', chalk.blue, 'world')).to.equal('hello ' + chalk.blue('world'));
+  });
+
+  it('translate by non existing key', function() {
+    expect(strings.translate('i18n:')).to.equal(':i18n-key-not-found');
+    expect(strings.translate('i18n:non.existing.key')).to.equal('non.existing.key:i18n-key-not-found');
+  });
+
+  it('translate by key', function() {
+    expect(strings.translate('i18n:test.0')).to.equal('test');
+
+    expect(strings.translate('i18n:test.1')).to.equal('test %s');
+    expect(strings.translate('i18n:test.1', 'this')).to.equal('test ' + chalk.red('this'));
+    expect(strings.translate('i18n:test.1', chalk.blue, 'this')).to.equal('test ' + chalk.blue('this'));
+
+    expect(strings.translate('i18n:test.2')).to.equal('test %s and %s');
+    expect(strings.translate('i18n:test.2', 'this')).to.equal('test ' + chalk.red('this') + ' and %s');
+    expect(strings.translate('i18n:test.2', 'this', 'that'))
+      .to.equal('test ' + chalk.red('this') + ' and ' + chalk.red('that'));
+
+    expect(strings.translate('i18n:test.2', chalk.green, 'this', 'that'))
+      .to.equal('test ' + chalk.green('this') + ' and ' + chalk.green('that'));
+
+    expect(strings.translate('i18n:test.2', chalk.green, 'this', chalk.yellow('that')))
+      .to.equal('test ' + chalk.green('this') + ' and ' + chalk.yellow('that'));
+  });
+
+});
