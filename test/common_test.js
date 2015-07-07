20d2c3c794538033e349244c7d23c01f5787692b
Arthur Bogaart <spikylee@gmail.com>
2015-07-07 16:10:50 +0200

    Common tests for parseBool and error

--- test/common_test.js
+++ test/common_test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var expect = require('chai').expect;
+var chalk  = require('chalk');
 var common = require('../lib/util/common.js');
 
 describe('Common utils', function() {
@@ -18,4 +19,27 @@ describe('Common utils', function() {
     expect(common.escape('escape me')).to.equal('escape\\ me');
     expect(common.escape('escape" me')).to.equal('escape\\"\\ me');
   });
+
+  it('create error with colored params', function() {
+    expect(common.error.bind(common, 'It is bad!')).to.throw('It is bad!');
+    expect(common.error.bind(common, 'It is %s!', 'bad')).to.throw('It is ' + chalk.red('bad') + '!');
+  });
+
+  it('parse bool', function() {
+    expect(common.parseBool()).to.equal(null);
+    expect(common.parseBool('test')).to.equal(null);
+
+    var yes = ['y', 'yes', 'ok', 'true'];
+    yes.forEach(function(b) {
+      expect(common.parseBool(b)).to.be.true;
+      expect(common.parseBool(b.toUpperCase())).to.be.true;
+    });
+
+    var no = ['n', 'no', 'cancel', 'false'];
+    no.forEach(function(b) {
+      expect(common.parseBool(b)).to.not.be.true;
+      expect(common.parseBool(b.toUpperCase())).to.not.be.true;
+    });
+  });
+
 });
