76f62d2be16f84c1bf10804ca688e4f3b6df953d
Arthur Bogaart <spikylee@gmail.com>
2015-07-11 02:16:49 +0200

    Added test for writeToFile

--- test/templates_test.js
+++ test/templates_test.js
@@ -1,17 +1,42 @@
 'use strict';
 
 var expect = require('chai').expect;
-var templates = require('../lib/util/templates.js');
+var sh     = require('shelljs');
+var files  = require('../lib/util/files.js');
+var tpls   = require('../lib/util/templates.js');
 
 describe('Template utils', function() {
   describe('#createFromFile', function() {
     it('should create a template from file', function() {
-      var tpl = templates.createFromFile('test.tpl', {value: 'test value'});
+      var tpl = tpls.createFromFile('test.tpl', {value: 'test value'});
       expect(tpl).to.equal('<test>test value</test>');
     });
     it('should throw an error when trying to create a template from an unknown file', function() {
-      var tpl = templates.createFromFile.bind(templates, 'unkown-file.tpl', {value: 'test value'});
+      var tpl = tpls.createFromFile.bind(tpls, 'unkown-file.tpl', {value: 'test value'});
       expect(tpl).to.throw(Error);
     });
+  });
+
+  describe('#writeToFile', function() {
+    it('should write a template to a file', function() {
+      var tpl = tpls.createFromFile('test.tpl', {value: 'test value'});
+      var path = 'test-tmp.txt';
+      tpls.writeToFile(tpl, path, true);
+      var newFile = files.readFileIfExists(path);
+      expect(newFile).to.equal('<test>test value</test>');
+      sh.rm(path);
+    });
+
+    it('should throw an error when trying to overwrite existing file', function() {
+      var tpl = tpls.createFromFile('test.tpl', {value: 'test value'});
+      var path = 'test-tmp.txt';
+      tpls.writeToFile(tpl, path, true);
+
+      var write = tpls.writeToFile.bind(tpls, tpl, path);
+      expect(write).to.throw(Error);
+
+      sh.rm(path);
+    });
+
   });
 });
